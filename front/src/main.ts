c4ed39b566e6f0c8b5a3db0c8f3a4a3cb6740fcb
Nulo <hi@nulo.in>
2024-08-29 22:56:28 -0300

    usar inter siempre
    fixes #2

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,8 +1,9 @@
-import './app.css'
-import App from './App.svelte'
+import "@fontsource/inter/400.css";
+import "./app.css";
+import App from "./App.svelte";
 
 const app = new App({
-  target: document.getElementById('app')!,
-})
+  target: document.getElementById("app")!,
+});
 
-export default app
+export default app;
