7eba9a28344c3c5dfa8a0d432da8aac61d9fdad1
Nulo <hi@nulo.in>
2024-08-29 21:55:15 -0300

    intentar arreglar mobile

--- front/src/lib/Magic.svelte
+++ front/src/lib/Magic.svelte
@@ -76,14 +76,8 @@
   let divEl: HTMLDivElement;
 
   function scrollToCenter() {
-    const vw = Math.max(
-      document.documentElement.clientWidth || 0,
-      window.innerWidth || 0,
-    );
-    const vh = Math.max(
-      document.documentElement.clientHeight || 0,
-      window.innerHeight || 0,
-    );
+    const vw = Math.max(document.documentElement.clientWidth || 0);
+    const vh = Math.max(document.documentElement.clientHeight || 0);
 
     window.scrollTo(
       divEl.clientWidth / 2 - vw / 2,
